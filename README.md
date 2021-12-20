2f4c1302acce80e2dd6073b0aa49264ed66d238c
Jason Kimmerling <jkimmerling@protonmail.com>
2021-12-20 09:24:16 -0500

    Refactoring

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # SimulacrIoT
 HVAC and IoT Device Simulator
 
-## This is meant to act as a simulator of a device/network of devices.
+This is meant to act as a simulator of a device/network of devices. Its primary uses cases are to test  
+either data pipelines or fault detection algorithms.
+
+# Work in progress  
+Currently just a basic Flask Api interface  
+Coming next:  
+MQTT & Bacnet interfaces
 
 Credit for Dataset:  
 The dataset used for this project: "Automated Fault Detection and Diagnostics Data Curation and Benchmarking"  
--- flaskWebApi.py
+++ flaskWebApi.py
@@ -1,71 +1,81 @@
 import csv
 import json
-import pandas as pd
 from datetime import datetime, timedelta
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 
-with open('config.json') as configFile:
-    config = json.load(configFile)
-    
-hostName = config['hostName']
-serverPort = config['port']
-csvFilePath = config['csvFilePath']
-interval = config['interval']
-debugMode = config['debugMode']
-debugDict = {}
-if debugMode:
-    debugDict['config'] = config
-
-startTime = datetime.now().replace(second=0, microsecond=0)
-
-dataFrame = pd.read_csv(csvFilePath)
+# startTime = datetime.now().replace(second=0, microsecond=0)
 
 # create the base flask app
 app = Flask(__name__)
 # create the api object to allow class creation
 api = Api(app)
+# count = 0
 
 class BaseTime(Resource):
+    count = 0
+    startTime = datetime.now()
+    def __init__(self, dataFrame, mainConfig, debugDict, count):
+        self.config = mainConfig
+        self.dataFrame = dataFrame
+        self.debugDict = debugDict
+        # self.count = count
+        self.offset = 0        
+        self.timeAtRun = datetime.now()          
     
     def getTimeDelta(self):
         timeNow = datetime.now()
-        tDeltaSeconds = timeNow - startTime
+        tDeltaSeconds = timeNow - BaseTime.startTime.replace(second=0, microsecond=0)
         tDeltaMinutes = tDeltaSeconds.total_seconds() // 60
         return tDeltaMinutes
-    
-    def intervalHandler(self):
-        timeAtRun = datetime.now().replace(second=0, microsecond=0)
-        timeDeltaS = timeAtRun - startTime
-        timeDeltaM = self.getTimeDelta()
-        remainder = int(timeDeltaM) % interval
-        publishTime = str(timeAtRun - timedelta(seconds=remainder*60))
-        offset = int(timeDeltaM) - remainder
-        if debugMode:
-            debugDict['startTime'] = str(startTime)
-            debugDict['timeAtRun'] = str(timeAtRun)
-            debugDict['timeDeltaMinutes'] = int(timeDeltaM)
-            debugDict['offset'] = offset
-        return {"publishTime": publishTime, "offset": offset}
-    
-    def formatTime(self, jsonRow, publishTime):
-        row = jsonRow
-        row['Datetime'] = publishTime
+
+    def getPublishTime(self):    
+        if self.config['postMode'] == 'perCallBased':
+            publishTime = str(self.timeAtRun)
+            timeDeltaM = 0
+        elif self.config['postMode'] == 'intervalBased':
+            timeDeltaM = self.getTimeDelta()
+            remainder = int(self.getTimeDelta()) % interval
+            publishTime = str(self.timeAtRun - 
+                            self.timedelta(seconds=remainder*60))
+            BaseTime.startTime = BaseTime.startTime.replace(second=0, microsecond=0)
+        if self.config['debugMode']:
+            self.debugDict['startTime'] = str(BaseTime.startTime)
+            self.debugDict['timeAtRun'] = str(self.timeAtRun)
+            self.debugDict['timeDeltaMinutes'] = int(timeDeltaM)
+        return publishTime
+
+    def getOffset(self):
+        if self.config['postMode'] == 'perCallBased':
+            offset = BaseTime.count
+            BaseTime.count += 1
+        elif self.config['postMode'] == 'intervalBased':
+            intTimeDeltaM = int(self.getTimeDelta())
+            remainder = intTimeDeltaM % self.config['interval']
+            offset = intTimeDeltaM - remainder
+        if self.config['debugMode']:
+            self.debugDict['offset'] = offset
+        return offset
+
+    def formatTime(self):
+        row = self.jsonRow
+        row['Datetime'] = self.publishTime
         return row
-        
-    def getJsonRow(self, offset):
-        singleRow = dataFrame.iloc[offset].to_dict()
+
+    def getJsonRow(self):
+        singleRow = self.dataFrame.iloc[self.offset].to_dict()
         return singleRow
-    
+
     def get(self):
-        intervalDict = self.intervalHandler()
-        jsonRow = self.getJsonRow(intervalDict["offset"])
-        if debugMode:
-            jsonRow["DEBUG"] = debugDict
-        formattedJsonRow = self.formatTime(jsonRow, intervalDict["publishTime"])
+        if self.config['postMode'] == 'perCallBased':
+            self.timeAtRun = datetime.now()
+        elif self.config['postMode'] == 'intervalBased':
+            self.timeAtRun = datetime.now().replace(second=0, microsecond=0)      
+        self.publishTime = self.getPublishTime()        
+        self.offset = self.getOffset()
+        self.jsonRow = self.getJsonRow()
+        if self.config['debugMode']:
+            self.jsonRow["DEBUG"] = self.debugDict
+        formattedJsonRow = self.formatTime()
         response = formattedJsonRow
         return response, 200
-
-api.add_resource(BaseTime, '/')
-
-app.run()
